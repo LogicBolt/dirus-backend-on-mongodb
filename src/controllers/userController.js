a72151350fb8e4e6d100aac21fe9c34b84eb74ca
LogicBolt <joeaska84@gmail.com>
2024-09-13 15:14:18 -0700

    updating point in real time

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -30,5 +30,32 @@ const getUser = async (req, res) => {
     return res.status(500).json({ error: 'Server error' });
   }
 };
-
-module.exports = { getUser };
+const updatePoint = async (req, res) => {
+    const { userId, value } = req.body;
+    try {
+        console.log("Received userId:", userId, value, );
+    
+        const user = await User.findOne({ userId });
+    
+        if (user) {
+            console.log("user is existed")
+          return res.json({ is_new: false });
+        } else {
+            console.log("new user coming")
+          // Create a new user since none was found
+          const newUser = new User({
+            userId,
+            userName,
+            is_new: true,
+            points: 500,
+            referer: startParam || 'self',  // Get the referer from headers if available
+          });
+          console.log(newUser)
+          await newUser.save();
+          return res.status(201).json({ is_new: true });
+        }
+      } catch (error) {
+        return res.status(500).json({ error: 'Server error' });
+      }
+}
+module.exports = { getUser,updatePoint };
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const { getUser } = require('../controllers/userController');
+const { getUser, updatePoint } = require('../controllers/userController');
 const router = express.Router();
 
 router.get('/:userId', getUser); // GET user by userId
-
+router.post('/update_point', updatePoint);
 module.exports = router;
